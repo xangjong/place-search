d8e366dbc37003bde92763e97b1d38f93fc537c8
xangjong <csj7080@naver.com>
2022-12-09 16:15:34 +0900

    Add place-search

---
+++ src/main/java/com/example/restaurant/db/MemoryDbEntity.java
@@ -0,0 +1,14 @@
+package com.example.restaurant.db;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MemoryDbEntity {
+
+    private Integer index;
+
+}
